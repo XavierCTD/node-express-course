0fcbce742b57468fcdef7b93b33d465b403f0820
XavierCTD <macfamlove01@gmail.com>
2025-12-11 18:18:36 -0500

    Express-tutorial Update #1.

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -3,14 +3,6 @@ const path = require('path');
 const { products } = require("./data");
 const app = express();
 
-const production = [
-  { id: 1, name: 'john' },
-  { id: 2, name: 'peter' },
-  { id: 3, name: 'susan' },
-  { id: 4, name: 'anna' },
-  { id: 5, name: 'emma' },
-];
-
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/api/v1/products', (req, res) => {
@@ -22,19 +14,19 @@ app.get('/api/v1/products', (req, res) => {
     res.json(newProducts)
 });
 
+app.get('/api/v1/test', (req, res) => {
+    res.status(200).json({ message: "It worked!"});
+});
+
 app.get('/api/v1/products/:productID', (req, res) => {
     const idToFind = parseInt(req.params.productID);
-    const product = production.find((p) => p.id === idToFind);
+    const product = products.find((p) => p.id === idToFind);
 
     if(!product) {
         return res.status(404).json({ message: "Product not found." });
     }
 
     res.json(product);
-});
-
-app.get('/api/v1/test', (req, res) => {
-    res.status(200).json({ message: "It worked!"});
 });
 
 app.get('/api/v1/query', (req, res) => {
